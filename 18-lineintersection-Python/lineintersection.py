9c0e82368e3ad967e5f0d29fb637525c5a7f14a2
dharshakch97-cloud <saidh1971>
2020-07-14 18:25:28 +0530

    Jul 14  18:25:28

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -8,5 +8,7 @@ def lineintersection(m1, b1, m2, b2):
 	# your code goes here
 	if (m1 == m2):
 		return None
+	elif m1 % m2 == 0 or m2 % m1 == 0:
+		return None
 	else:
 		return (b2 - b1)/(m1 - m2)
