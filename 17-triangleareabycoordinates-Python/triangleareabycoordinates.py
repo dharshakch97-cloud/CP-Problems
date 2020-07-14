394190216fed0740c9c32ee222a017e4aace89e6
dharshakch97-cloud <saidh1971>
2020-07-14 18:02:17 +0530

    Jul 14  18:02:17

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -4,6 +4,35 @@
 # area of the triangle formed by those three points. Hint: you should make constructive use of
 # the triangleArea function you just wrote above.
 
+import math
+
+def isLegaltriangle(s1, s2, s3):
+	if s1 <= 0 or s2 <= 0 or s3 <= 0:
+		return False
+	max_side = max(s1, s2, s3)
+	if (max_side == s1 and s2 + s3 > s1):
+		return True
+	elif (max_side == s2 and s1 + s3 > s2):
+		return True
+	elif (max_side == s3 and s1 + s2 > s3):
+		return True
+	else:
+		return False
+
+def trianglearea(s1, s2, s3):
+	# your code goes here
+	if isLegaltriangle(s1, s2, s3):
+		s = (s1+s2+s3)/2
+		return math.sqrt(s*(s-s1)*(s-s2)*(s-s3))
+	else:
+		return 0
+
+def distance(x1, y1, x2, y2):
+	return math.sqrt((x2 - x1)**2 + ((y2 - y1)**2))
+
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a = distance(x1, y1, x2, y2)
+	b = distance(x2, y2, x3, y3)
+	c = distance(x3, y3, x1, y1)
+	return trianglearea(a, b, c)
