bcc1230237ae98299fc82a5dc14a5412b4d7ee9b
dharshakch97-cloud <saidh1971>
2020-07-16 15:27:33 +0530

    Jul 16  15:27:33

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -8,13 +8,16 @@ def smallestdifference(a):
 	# Your code goes here
 	if len(a) == 1:
 		return a[0]
-	l = sorted(a)
-	d = l[1] - l[0]
-	for i in range(1, len(a) - 1):
-		d1 = a[i + 1] - a[i]
-		if d > d1:
-			d = d1
-	return d
+	elif len(a) > 0:
+		l = sorted(a)
+		d = l[1] - l[0]
+		for i in range(1, len(a) - 1):
+			d1 = a[i + 1] - a[i]
+			if d > d1:
+				d = d1
+		return d
+	else:
+		return -1
 
 print(smallestdifference([3, -7, 0]))
 print(smallestdifference([-59,-36,-13,1,-53,-92,-2,-96,-54,75]))
