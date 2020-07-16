af7c15871c2b756d9ce5804909da0949106f325c
dharshakch97-cloud <saidh1971>
2020-07-16 10:59:21 +0530

    Jul 16  10:59:21

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -10,6 +10,40 @@
 # assert(nthHappyNumber(6) == 28)
 # assert(nthHappyNumber(7) == 31)
 
+def squares(n):
+	sum = 0
+	while n > 0:
+		r = n % 10
+		sum += r * r
+		n = n // 10
+	return sum
+
+def ishappynumber(n):
+	# your code goes here
+	numList = list()
+	if n == 1:
+		return True
+	elif n > 0:
+		while True:
+			n = squares(n)
+			if n == 1:
+				return True
+			elif n in numList:
+				return False
+			else:
+				numList.append(n)
+	else:
+		return False
 
 def fun_nth_happy_number(n):
-	return 0
+	count = 0
+	if n > 0:
+		while count != n:
+			while True:
+				i += 1
+				if ishappynumber(i):
+					break
+			count += 1
+		return i
+	else:
+		return 1
