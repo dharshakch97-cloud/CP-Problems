40e1f2a4ab22250760ef1690bd0683846a9f0757
dharshakch97-cloud <saidh1971>
2020-07-16 13:06:00 +0530

    Jul 16  13:06:00

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -3,7 +3,13 @@
 # replace(s1, s2, s3) that returns a string equal to 
 # s1.replace(s2, s3), but again without calling s.replace().
 
+'''
+	s1 = helloworld123 s2 = hello s3 = 345
+'''
 
 def fun_replace(s1, s2, s3):
+	str = s1.split(s2)
+	print(str)
 	return s1
 
+fun_replace("helloworld123", "hello", "345")
