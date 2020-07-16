4480dfe801a3fecbea70e7c82d4d45b0e20690bf
dharshakch97-cloud <saidh1971>
2020-07-16 11:34:38 +0530

    Jul 16  11:34:38

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -8,5 +8,20 @@
 
 
 def fun_interleave(s1,s2):
+	str = ""
+	if len(s1) == len(s2):
+		for i in range(len(s1)):
+			s += s1[i] + s2[i]
+		return str
+	elif len(s1) > len(s2):
+		for i in range(len(s2)):
+			str += s2[i] + s1[i]
+		str += s1[len(s2):len(s1):1]
+		return str
+	elif len(s2) > len(s1):
+		for i in range(len(s1)):
+			str += s2[i] + s1[i]
+		str += s2[len(s1):len(s2):1]
+		return str
 	return ""
 	
