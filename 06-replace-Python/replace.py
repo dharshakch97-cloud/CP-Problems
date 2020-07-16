ce541263ecb0aac259916db617f48ac9723ea438
dharshakch97-cloud <saidh1971>
2020-07-16 13:12:36 +0530

    Jul 16  13:12:36

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -9,8 +9,10 @@
 
 def fun_replace(s1, s2, s3):
 	str = s1.split(s2)
-	print(str)
+	if len(str[0]) == len(s1):
+		return s1
 	return s1
 
 fun_replace("helloworld123", "hello", "345")
 fun_replace("helloworld123", "123", "345")
+fun_replace("helloworld123", "34567", "345")
