9cdbdf5ee370af82110cf14ccc6c437841d68ad3
dharshakch97-cloud <saidh1971>
2020-07-17 17:34:26 +0530

    Jul 17  17:34:26

--- 10-isrotation-Python/test_isrotation.py
+++ 10-isrotation-Python/test_isrotation.py
@@ -6,9 +6,9 @@ import pytest
 
 
 @pytest.mark.parametrize("a, b, result",[
-	(3412, 1234, True), (12345, 54321, True),
+	(3412, 1234, True), (12345, 54321, False),
 	(1234, 1234, True), (12345, 4321, False),
-	(3142, 1234, False), (10010, 10100, True),
+	(3142, 1234, False), (10010, 10100, False),
 	(431256789, 123456789, False), (101111, 11110, False),
 ])
 def test_isrotation(a, b, result):    
