c7427a0616f5932a1061972068c5786a0c4d0e15
dharshakch97-cloud <saidh1971>
2020-07-17 16:37:44 +0530

    Jul 17  16:37:44

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -4,7 +4,14 @@
 # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
 
 
-
+def isprime(n):
+	for i in range(2, n//2):
+		if  n%i == 0: return True
+	return False
 
 def fun_nth_additive_prime(n):
-	return 1
+	nums = list(map(int, str(n)))
+	s = sum(nums)
+	if isprime(s):
+		return True
+	return False
