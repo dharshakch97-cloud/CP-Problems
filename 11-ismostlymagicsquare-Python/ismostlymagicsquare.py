a2c5b94184c51818ec63b5ffe86d23faaf56f4ca
dharshakch97-cloud <saidh1971>
2020-07-18 20:11:39 +0530

    Jul 18  20:11:39

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -32,3 +32,5 @@ def ismostlymagicsquare(a):
 	return False
 
 print(ismostlymagicsquare([[2, 7, 6], [9, 5, 1], [4, 3, 8]]))
+print(ismostlymagicsquare([[16, 3, 2, 13], [5, 10, 11, 8], [9, 6, 7, 12],[4, 15, 14, 1]]))
+print(ismostlymagicsquare([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
