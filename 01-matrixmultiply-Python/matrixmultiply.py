383f05dd3efeedb0161306dea0bdd11806d3fefe
dharshakch97-cloud <saidh1971>
2020-07-21 10:58:22 +0530

    matrixmultiply done

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,7 +5,12 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
+    res = [[sum(a * b for a, b in zip(i, j)) 
+            for j in zip(*m2)] for i in m1]
+    return res
+
+print(fun_matrixmultiply([[12, 7, 3], [4, 5, 6], [7, 8, 9]], 
+    [[5, 8, 1, 2], [6, 7, 3, 0], [4, 5, 9, 1]]))
 
 
 
