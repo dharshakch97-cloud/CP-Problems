f8478596563a13577eb3198622b0be88f65409b4
dharshakch97-cloud <saidh1971>
2020-07-23 12:51:33 +0530

    nth_lefttruncatable done

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -10,40 +10,41 @@
 
 import math
 def isprime(n):
-    for i in range(2, n):
-        if n%i == 0:
-            return False
-    return True
+    if n > 1:
+        for i in range(2, n):
+            if n%i == 0:
+                return False
+        return True
 
 def lefttruncatebyone(n):
-    if n < 10 and isprime(n):
-        return True
-    else:
-        count = 0
-        s = str(n)
-        while len(s) != 0:
-            if isprime(int(s)):
-                s = s[1:]
-            count += 1
-        if count == len(str(n)):
+    if "0" not in str(n) and isprime(n):
+        if n < 10:
             return True
+        n = str(n)
+        n = int(n[1:len(n)])
+        # print(n)
+        if isprime(n):
+            return lefttruncatebyone(n)
         return False
+    return False
 
-def fun_nth_lefttruncatableprime(n):
-    if n == 0:
-        return 2                   #  3 -> 1, 5 -> 2, 7 -> 3, 13 -> 4, 17 
-    i = -1
-    j = 2
-    while i < n:
-        if lefttruncatebyone(j):
-            i += 1
-        j += 1
-    return j - 1
+def fun_nth_lefttruncatableprime(n): 
+    i = 2
+    j = -1
+    while True:
+        if lefttruncatebyone(i):
+            j += 1
+        if j == n:
+            return i
+        i += 1
 
 # print(lefttruncatebyone(35))
 print(fun_nth_lefttruncatableprime(0))
 print(fun_nth_lefttruncatableprime(1))
-print(fun_nth_lefttruncatableprime(2))
-print(fun_nth_lefttruncatableprime(3))
-print(fun_nth_lefttruncatableprime(4))
 print(fun_nth_lefttruncatableprime(5))
+print(fun_nth_lefttruncatableprime(10))
+print(fun_nth_lefttruncatableprime(15))
+print(fun_nth_lefttruncatableprime(20))
+print(fun_nth_lefttruncatableprime(25))
+
+# n = 5,  j = 4 i = 13
