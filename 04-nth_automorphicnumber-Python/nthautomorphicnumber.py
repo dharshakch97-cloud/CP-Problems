aaebbc72ef225fbaa479f14ffa4678aefc588e16
dharshakch97-cloud <saidh1971>
2020-07-24 12:01:28 +0530

    tests not done

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -3,6 +3,29 @@
 # number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 # 76 and 890625 are all automorphic numbers.
 
+def isautomorphic(n):
+	sq = n**2
+	last = sq%pow(10, len(str(n)))
+	# print(last)
+	if last == n:
+		return True
+	return False
+	
 def nthautomorphicnumbers(n):
 	# Your code goes here
-	pass
+	i = 0
+	count = 0
+	while True:
+		if isautomorphic(i):
+			count += 1
+			if count == n:
+				return i
+		i += 1
+
+print(isautomorphic(0))
+print(nthautomorphicnumbers(1))
+print(nthautomorphicnumbers(2))
+print(nthautomorphicnumbers(3))
+print(nthautomorphicnumbers(14))
+print(nthautomorphicnumbers(15))
+print(nthautomorphicnumbers(16))
