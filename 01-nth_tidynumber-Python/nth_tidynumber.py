2c993a0840c3fd0d48c93e3524f4f9c370c8b2b8
dharshakch97-cloud <saidh1971>
2020-07-24 10:48:16 +0530

    nth_tidynumber done

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,33 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
+def istidy(n):
+    n = str(n)
+    s = sorted(list(map(int, n)))
+    r = ""
+    for i in s:
+        r += str(i)
+    return n == r
+    
 def fun_nth_tidynumber(n):
-    return 0
+    count = -1
+    i = 1
+    while True:
+        if istidy(i):
+            count += 1
+            if count == n:
+                return i
+        i += 1
+    # return 0
+
+print(istidy(1))
+# print(istidy(34567))
+print(fun_nth_tidynumber(0))
+print(fun_nth_tidynumber(1))
+print(fun_nth_tidynumber(5))
+print(fun_nth_tidynumber(15))
+print(fun_nth_tidynumber(35))
+print(fun_nth_tidynumber(50))
+print(fun_nth_tidynumber(100))
+print(fun_nth_tidynumber(250))
+print(fun_nth_tidynumber(500))
