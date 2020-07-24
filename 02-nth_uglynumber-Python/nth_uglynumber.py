80c0a07ab4ff225efc7c0048c31124a163fb4e1e
dharshakch97-cloud <saidh1971>
2020-07-24 11:33:56 +0530

    nth_uglynumber done

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -4,4 +4,32 @@
 
 
 def fun_nth_uglynumber(n):
-    return 0
+    ugly = [0] * (n+1)
+    ugly[0] = 1
+    i = j = k = 0
+
+    next2 = 2
+    next3 = 3
+    next5 = 5
+
+    for m in range(1, n+1):
+        ugly[m] = min(next2, next3, next5)
+        if ugly[m] == next2:
+            i += 1
+            next2 = ugly[i]*2
+        if ugly[m] == next3:
+            j += 1
+            next3 = ugly[j]*3
+        if ugly[m] == next5:
+            k += 1
+            next5 = ugly[k]*5
+    return ugly[-1]
+
+print(fun_nth_uglynumber(0))
+print(fun_nth_uglynumber(1))
+print(fun_nth_uglynumber(2))
+print(fun_nth_uglynumber(5))
+print(fun_nth_uglynumber(10))
+print(fun_nth_uglynumber(25))
+print(fun_nth_uglynumber(50))
+print(fun_nth_uglynumber(100))
