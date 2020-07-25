7828c037dcd9289e461195cbb49e7b99aa771e81
dharshakch97-cloud <saidh1971>
2020-07-25 16:56:03 +0530

    Jul 25  16:56:03

--- 08-getallpermutations-Python/getallpermutations.py
+++ 08-getallpermutations-Python/getallpermutations.py
@@ -2,18 +2,23 @@
 # Write an efficient program to print all permutations of a given String. For example, if given input is "abc" then 
 # your program should print all 6 permutations e.g. [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
 
-def getpermutation(a, i, j):
-	if i == j:
-		return a
-	else:
-		for p in xrange(i, j+1):
-			a[i], a[p] = a[p], a[i]
-			getpermutation(a, i+1, j)
-			a[i], a[p] = a[p], a[i]
-
+def getpermutation(s):
+	if len(s) == 0:
+		return []
+	if len(s) == 1:
+		return [s]
+	
+	l = []
+	for i in range(len(s)):
+		p = s[i]
+		rem = s[:i] + s[i+1:]
+		for q in getpermutation(rem):
+			l.append([p] + q)
+	return l
+	
 def getallpermutations(x):
 	# Your code goes here
 	s = list(x)
-	return getpermutation(s, 0, len(x)-1)
+	return 
 
 getallpermutations("ABC")
