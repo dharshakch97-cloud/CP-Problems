597796b0729a8a7e6e8c3b24953ec281a170d3bc
dharshakch97-cloud <saidh1971>
2020-07-26 15:20:52 +0530

    Jul 26  15:20:52

--- 07-graphrepresentation-Python/test_solution.py
+++ 07-graphrepresentation-Python/test_solution.py
@@ -11,15 +11,15 @@ graph.insert_edge(102, 1, 4)
 graph.insert_edge(103, 3, 4)
 
 
-@pytest.mark.parametrize("result",[([(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)])])
+@pytest.mark.parametrize("result",[(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)])
 def test_get_edge_list(result):    
     assert graph.get_edge_list() == result
 
-@pytest.mark.parametrize("result",[([None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None])])
+@pytest.mark.parametrize("result",[None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None])
 def test_get_adjacency_list(result):    
     assert graph.get_adjacency_list() == result
 
-@pytest.mark.parametrize("result",[([[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]])])
+@pytest.mark.parametrize("result",[[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]])
 def test_get_adjacency_matrix(result):    
     assert graph.get_adjacency_matrix() == result
 
