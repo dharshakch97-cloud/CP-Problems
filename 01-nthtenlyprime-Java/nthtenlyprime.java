e2ce324359762fed269d8f1ee205cf06f56bbeb3
dharshakch97-cloud <saidh1971>
2020-07-27 18:02:22 +0530

    Jul 27  18:02:22

--- 01-nthtenlyprime-Java/nthtenlyprime.java
+++ 01-nthtenlyprime-Java/nthtenlyprime.java
@@ -5,7 +5,43 @@
 
 
 class nthtenlyprime {
+	public boolean isPrime(int n) {
+		if (n <= 1)
+			return false;
+		for (int i = 2; i < n; i++)
+			if (n%i == 0)
+				return false;
+		return true;
+	}
+
+	public boolean istenly(int n) {
+		int m = n;
+		int s = 0;
+		while (n!=0) {
+			int r = n%10;
+			n = n/10;
+			s += r;
+		}
+		if (s == 10)
+			return true;
+		return false;
+	}
+
 	public int fun_nthtenlyprime(int n){
-		return 1;
+		int i = 2, c = 0;
+		while (c <= n) {
+			if (isPrime(i) && istenly(i)) {
+				c++;
+			}
+			i++;
+		}
+		return i-1;
+	}
+
+	public static void main(String[] args) {
+		nthtenlyprime t = new nthtenlyprime();
+		System.out.println(t.fun_nthtenlyprime(1));
+		System.out.println(t.fun_nthtenlyprime(4));
+		System.out.println(t.fun_nthtenlyprime(10));
 	}
 }
